f1101b2845249bb164440cf39f0f1034c1f0ff0d
Angys <32882213+GirlLollipop@users.noreply.github.com>
2022-12-03 11:23:06 -0600

    Archivos portafolio tecnolochicas

---
+++ main.js
@@ -0,0 +1,17 @@
+//alert("Hola");
+  
+
+//Este es el código para la animación
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
